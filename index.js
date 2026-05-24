137474161c85d5fa281ca355dcea67690e3dd060
joraboyevfayzullo46-gif <joraboyevfayzullo46@gmail.com>
2026-05-24 20:37:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
 
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Bot ishlayapti 🚀');
+    bot.sendMessage(
+        msg.chat.id,
+        'Link yuboring 📥\n\nInstagram / TikTok / YouTube / Facebook'
+    );
+});
+
+bot.on('message', async (msg) => {
+
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (!text.startsWith('http')) return;
+
+    try {
+
+        bot.sendMessage(chatId, 'Yuklanmoqda... ⏳');
+
+        const api = `https://api.siputzx.my.id/api/d/ytmp4?url=${text}`;
+
+        const response = await axios.get(api);
+
+        const video = response.data.data.dl;
+
+        bot.sendVideo(chatId, video);
+
+    } catch (err) {
+
+        console.log(err);
+
+        bot.sendMessage(chatId, 'Xatolik chiqdi ❌');
+
+    }
+
 });
